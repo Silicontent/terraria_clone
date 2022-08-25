6edef599c4ec44c230d2b71f4b87dd85b47a03fe
Silicontent <61814046+Silicontent@users.noreply.github.com>
2022-08-24 21:52:00 -0500

    Initial branch commit

--- scripts/WorldGen.gd
+++ scripts/WorldGen.gd
@@ -14,65 +14,91 @@ onready var selector: Sprite = $Selector
 onready var mouse_light: Light2D = $Selector/Light2D
 
 
-# WORLD ELEVATION GENERATION -----------------------------
-# Surface functions
-func generate_surface():
-	set_noise_prop(0, 5, 0.1, 1.43)
+# WORLD ELEVATION GENERATION ---------------------------------------------
+func gen_base():
+	for x in world_x:
+		for y in surf_y:
+			var tile_id = 0
+			set_cell(x, y, tile_id)
+	
+	for x in world_x:
+		for y in ug_y:
+			var tile_id = 1
+			set_cell(x, surf_y+y, tile_id)
+
+
+func gen_splotches():
+	set_noise_prop(0, 5, 0.588, 2.43)
 	
 	for x in world_x:
 		for y in surf_y:
-			var tile_id = generate_surf_id(noise.get_noise_2d(x, y))
-			if tile_id != -1:
-				set_cell(x, y, tile_id)
+			var tile_id = gen_splotch_id(noise.get_noise_2d(x, y), 0, 1)
+			set_cell(x, y, tile_id)
 	
 	for x in world_x:
-		set_cell(x, 0, 3)
-
-func generate_surf_id(noise_level: float):
-	var tile_to_gen = -1
-	var rand_num = randi() % 100 + 1
-	
-	if rand_num <= 95:
-		tile_to_gen = 0
-	elif rand_num >= 96:
-		tile_to_gen = 1
-	
-	return tile_to_gen
+		for y in ug_y:
+			var tile_id = gen_splotch_id(noise.get_noise_2d(x, y), 1, 0)
+			set_cell(x, surf_y+y, tile_id)
 
 
-# Underground functions
-func generate_underground():
+func gen_caves():
 	set_noise_prop(0, 5, 0.588, 2.43)
 	
 	for x in world_x:
 		for y in ug_y:
-			var tile_id = generate_ug_id(noise.get_noise_2d(x, y))
-			if tile_id != -1:
-				set_cell(x, surf_y+y, tile_id)
+			var tile_id = gen_cave_id(noise.get_noise_2d(x, y))
+			set_cell(x, surf_y+y, tile_id)
 
-func generate_ug_id(noise_level: float):
-	# uses random numbers to mix stone and mud around the world
-	var tile_to_gen = -1
-	var rand_num = randi() % 100 + 1
+
+func gen_surface():
+	for x in world_x:
+		var current_tile = get_cell(x, 0)
+		if current_tile != -1:
+			set_cell(x, 0, 3)
+
+
+func gen_system():
+	set_noise_prop(1, 114.2, 0.266, 0.69)
 	
-	if rand_num <= 60:
-		tile_to_gen = 0
-	elif rand_num >= 61 and rand_num != 100:
-		tile_to_gen = 1
-	elif rand_num == 100:  # small chance of natural wood
-		tile_to_gen = 2
+	for x in world_x:
+		for y in world_y:
+			var tile_id = gen_system_id(noise.get_noise_2d(x, y))
+			set_cell(x, y, tile_id)
+
+
+# functions for getting tile IDs
+func gen_splotch_id(noise_level: float, splotch_tile: int, main_tile: int):
+	var tile_to_gen = splotch_tile
 	
-	# adds air into the world for caves, returns value to generate_level()
-	if noise_level <= -0.1:
+	if noise_level <= -0.2:
+		return main_tile
+	else:
+		return splotch_tile
+
+func gen_cave_id(noise_level: float): 
+	if noise_level <= -0.2:
 		return -1
 	else:
-		return tile_to_gen
+		return 1
+
+func gen_system_id(noise_level: float): 
+	if noise_level <= -0.2:
+		return -1
+	else:
+		return 1
 
 
-# World functions
+# WORLD FUNCTIONS --------------------------------------------------------
 func generate_level():
-	generate_surface()
-	generate_underground()
+	randomize()
+	noise.seed = randi()
+	print("World seed: ", noise.seed)
+	
+	gen_base()
+	gen_splotches()
+	gen_caves()
+	gen_surface()
+	gen_system()
 
 func clear_level():
 	for x in world_x:
@@ -80,37 +106,34 @@ func clear_level():
 			set_cell(x, y, -1)
 
 func set_noise_prop(oct: int, per: int, pers: float, lac: float):
-	randomize()
-	noise.seed = randi()
 	noise.octaves = oct
 	noise.period = per
 	noise.persistence = pers
 	noise.lacunarity = lac
 
 
-# SIGNALS
+# SIGNALS ----------------------------------------------------------------
 func _on_GenLevel_pressed():
 	clear_level()
 	generate_level()
 
 
-# BREAKING / PLACING
+# BREAKING / PLACING -----------------------------------------------------
 func _on_Player_mine():
-	var tile : Vector2 = world_to_map(selector.mouse_pos * 8) # gets the tile we clicked on
+	var tile: Vector2 = world_to_map(selector.mouse_pos * 8) # gets the tile we clicked on
 	var tile_id = get_cellv(tile) # returns the ID of that cell
 	
 	var new_id = -1
 	
-	if(tile_id != -1): # we clicked on a mud block
+	if tile_id != -1: # we clicked on a mud block
 		new_id = -1
 	set_cellv(tile, new_id)
-
 
 func _on_Player_place():
 	var tile: Vector2 = world_to_map(selector.mouse_pos * 8)
 	set_cellv(tile, 2)
 
 
-# OTHER FUNCTIONS
+# OTHER FUNCTIONS --------------------------------------------------------
 func _ready() -> void:
 	generate_level()
