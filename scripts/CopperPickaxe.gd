4edca0929b748d55138941d8712c1f417c586a58
Silicontent <61814046+Silicontent@users.noreply.github.com>
2022-08-28 22:08:02 -0500

    Updated tile and player sprites

--- scripts/CopperPickaxe.gd
+++ scripts/CopperPickaxe.gd
@@ -5,14 +5,20 @@ onready var anim_player = $AnimationPlayer
 var can_swing = true
 
 
+func _ready() -> void:
+	hide()
+
+
 func _physics_process(delta):
 	if Input.is_action_pressed("mb_left") and can_swing:
+		show()
 		anim_player.play("swing")
 		can_swing = false
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "swing":
+		hide()
 		emit_signal("mine")
 		anim_player.play("RESET")
 		can_swing = true
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -14,6 +14,8 @@ var speed := 100
 var jump_speed := -300
 var velocity := Vector2.ZERO
 
+onready var sprite: AnimatedSprite = $AnimSprite
+
 
 func _ready() -> void:
 	to_spawn()
@@ -22,18 +24,23 @@ func to_spawn():
 	global_position = start_pos
 
 
+# MOVEMENT ---------------------------------------------------------------
 func movement():
 	var dir = 0
 	
 	if Input.is_action_pressed("mv_left"):
 		dir -= 1
+		sprite.flip_h = true
 	if Input.is_action_pressed("mv_right"):
 		dir += 1
+		sprite.flip_h = false
 	
 	if dir != 0:
 		velocity.x = lerp(velocity.x, dir * speed, ACC)
+		sprite.play("walking")
 	else:
 		velocity.x = lerp(velocity.x, 0, FRIC)
+		sprite.play("idle")
 
 
 func _physics_process(delta):
@@ -42,16 +49,21 @@ func _physics_process(delta):
 	velocity.y += GRAV * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
+	
 	if Input.is_action_just_pressed("mv_jump"):
 		if is_on_floor():
 			velocity.y = jump_speed
+			sprite.play("jump")
 	
+	if Input.is_action_just_pressed("mb_left"):
+		sprite.play("mine")
 	if Input.is_action_just_pressed("mb_right"):
 		emit_signal("place")
 	if Input.is_action_just_pressed("util_enter"):
 		to_spawn()
 
 
+# SIGNALS ----------------------------------------------------------------
 func _on_GenLevel_pressed():
 	to_spawn()
 
